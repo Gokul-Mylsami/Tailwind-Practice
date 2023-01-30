674dbc24cce18943e0d35ce911ec55094119d78b
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-01-30 08:29:38 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/hero";
+import Content from "./components/Content";
+import Footer from "./components/footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
